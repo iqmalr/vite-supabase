d2869532c5a51ab6a41568df1f721e20ad23490b
iqmalr <m.iqmal.riffai@gmail.com>
2024-04-20 14:36:32 +0700

    Add supabaseURL

--- src/createClient.js
+++ src/createClient.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY
-);
+
+const supabaseUrl = "https://ozlxtwgypizcyiioxbqh.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96bHh0d2d5cGl6Y3lpaW94YnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyNDkzMzUsImV4cCI6MjAyODgyNTMzNX0.AkKTVQbMOJpGdOOvN_h1rmLUaxANNyQPmSyP_vZ_72k";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
